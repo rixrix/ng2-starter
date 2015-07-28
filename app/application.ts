4124c323712adc445be91a7b3ef61012505d853d
rix <rix@rixs-MacBook-Pro.local>
2015-07-29 09:43:03 +1200

    minor clean up

--- app/application.ts
+++ app/application.ts
@@ -1,8 +1,12 @@
-import {Component, View} from 'angular2/angular2';
+import {
+    Component,
+    View
+} from 'angular2/angular2';
+
 import {
     RouteConfig,
     RouterOutlet,
-    routerDirectives
+    RouterLink
 } from 'angular2/router';
 
 import {Main} from './main/main';
@@ -10,20 +14,16 @@ import {Main} from './main/main';
 @Component({
     selector: 'app'
 })
-
 @View({
-    directives: [
-        routerDirectives
-    ],
     template: `
         <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#!/">Stencil</a>
+                    <a class="navbar-brand" href="/">Stencil</a>
                 </div>
                 <div id="navbar" class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#!/">Home</a></li>
+                        <li class="active"><a href="/">Home</a></li>
                         <li><a href="#about">About</a></li>
                     </ul>
                 </div>
@@ -33,13 +33,11 @@ import {Main} from './main/main';
         <router-outlet></router-outlet>
     `
 })
-
 @RouteConfig([
     { path: '/', as: 'home', component: Main }
 ])
 
 export class AppEntryPoint {
     constructor() {
-        // empty
     }
 }
--- app/main/main.ts
+++ app/main/main.ts
@@ -1,4 +1,7 @@
-import {Component, View} from 'angular2/angular2';
+import {
+    Component,
+    View
+} from 'angular2/angular2';
 
 @Component({
     selector: 'main'
