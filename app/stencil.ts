f79fe09df5a7ea5d53ca3c1fc7bd0689900615a1
Richard Sentino <richard@mingdinative.com>
2015-07-30 22:44:07 +1200

    wip on material design lite

--- app/stencil.ts
+++ app/stencil.ts
@@ -1,5 +1,6 @@
 /// <reference path="libs.d.ts" />
 require('./assets/css/material.css');
+require('./styles/index.css');
 
 import {bootstrap} from 'angular2/angular2';
 import {routerInjectables} from 'angular2/router';
