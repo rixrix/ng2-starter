c4f6f48a212e4b255b64df124cee38e411bcafc2
Richard Sentino <richard@mingdinative.com>
2015-08-12 23:34:52 +1200

    fix 404 on page refresh due to issue w/ html5 strategy - hash strategy for now

--- app/stencil.ts
+++ app/stencil.ts
@@ -4,13 +4,19 @@ require('./assets/css/material.css');
 require('./styles/index.css');
 
 import {
-    bootstrap
+    bootstrap,
+    bind
 } from 'angular2/angular2';
 
-import {routerInjectables} from 'angular2/router';
+import {
+    routerInjectables,
+    LocationStrategy,
+    HashLocationStrategy
+} from 'angular2/router';
 
 import {AppEntryPoint} from './application'
 
 bootstrap(AppEntryPoint, [
-    routerInjectables
+    routerInjectables,
+    bind(LocationStrategy).toClass(HashLocationStrategy)
 ]);
