c2ff6baaeb468b7e8a38d3e5f9fafa16816feb9c
Richard Sentino <richard@mingdinative.com>
2015-08-21 10:06:44 +1200

    wip on 2.0.0-alpha.35

--- app/libs.d.ts
+++ app/libs.d.ts
@@ -1,4 +1,9 @@
-///<reference path='../typings/tsd.d.ts'/>
+///<reference path='../node_modules/angular2/bundles/typings/angular2/angular2.d.ts'/>
+///<reference path='../node_modules/angular2/bundles/typings/angular2/http.d.ts'/>
+///<reference path='../node_modules/angular2/bundles/typings/angular2/router.d.ts'/>
+///<reference path='../node_modules/angular2/bundles/typings/es6-promise/es6-promise.d.ts'/>
+///<reference path='../node_modules/angular2/bundles/typings/rx/rx.d.ts'/>
+///<reference path='../node_modules/angular2/bundles/typings/rx/rx-lite.d.ts'/>
 
 /**
  * Custom types for Webpack
